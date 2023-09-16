0bd480b7e80b69b6a0ad7233aa2870d4e147702c
Briiizero <rafaelpagliari15@hotmail.com>
2023-09-16 16:17:14 -0300

    Rota encontrada

--- app.js
+++ app.js
@@ -12,45 +12,68 @@ const pool = new Pool({
   port: 5432, // Porta padrão do PostgreSQL
 });
 
-// Configuração do Express para usar EJS como mecanismo de template
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
 // Importe a lógica de busca de rotas
 const routeFinder = require('./routeFinder');
+
+// Configuração do mecanismo de template EJS
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 // Rota para renderizar a página inicial
 app.get('/', async (req, res) => {
   try {
-    const query = 'SELECT * FROM locals'; // Substitua 'locals' pelo nome da sua tabela
-    const { rows } = await pool.query(query);
-
-    res.render('index.ejs', { locals: rows });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro interno do servidor');
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM locals');
+    const locals = result.rows;
+    client.release();
+    res.render('index.ejs', { locals });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro no servidor');
   }
 });
 
 // Rota para processar o formulário de busca de rotas
 app.get('/directions', async (req, res) => {
+  const originId = req.query.origem;
+  const destinationId = req.query.destino;
+
   try {
-    const origemId = parseInt(req.query.origem);
-    const destinoId = parseInt(req.query.destino);
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM locals');
+    const locals = result.rows;
+    
+    // Substitua 'yourGraph' pelo seu grafo real
+    const yourGraph = {
+      // Defina a estrutura do seu grafo aqui
+    };
 
-    // Use a lógica de busca de rotas do módulo routeFinder
-    const shortestPath = await routeFinder.findShortestPath(origemId, destinoId);
+    const shortestPath = routeFinder.dijkstra(yourGraph, originId, destinationId);
+    const distance = calculateDistance(yourGraph, shortestPath);
+    client.release();
 
-    // Renderize a página com as direções ou a representação visual da rota
-    res.render('directions.ejs', { shortestPath });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Erro interno do servidor');
+    res.render('directions.ejs', { shortestPath, distance });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro no servidor');
   }
 });
 
+// Função para calcular a distância da rota
+function calculateDistance(graph, path) {
+  let distance = 0;
+
+  for (let i = 0; i < path.length - 1; i++) {
+    const currentNode = path[i];
+    const nextNode = path[i + 1];
+    distance += graph[currentNode][nextNode];
+  }
+
+  return distance;
+}
+
 // Inicie o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor está rodando na porta ${port}`);
 });
 
